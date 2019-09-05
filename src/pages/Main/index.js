59ffb77e737150fbb036e40d1c1ed574fc2f4b16
Helton Denis <heltondeniss@gmail.com>
2019-09-04 21:02:03 -0300

    Realizando navegação

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import { Keyboard, ActivityIndicator } from 'react-native';
 import { Container, Form, Input, SubmitButton,
    List, User, Avatar, Name, Bio, ProfileButton, ProfileButtonText, Button, Text } from './styles';
@@ -8,6 +9,16 @@ import api from '../../services/api';
 
 
 export default class Main extends Component {
+
+ static navigationOptions = {
+    title: 'Usuários',
+  };
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      navigate: PropTypes.func,
+    }).isRequired,
+  };
 
   state = {
     newUser: '',
@@ -21,8 +32,8 @@ export default class Main extends Component {
       if(users) {
         this.setState({ users: JSON.parse(users) });
       }
-    })()
-
+    })(
+    )
   }
 
   componentDidUpdate(_, prevState) {
@@ -31,6 +42,12 @@ export default class Main extends Component {
      if(prevState.users !== users) {
       AsyncStorage.setItem('users', JSON.stringify(users));
      }
+  }
+
+  handleNavigate = (user) => {
+    const { navigation } = this.props;
+
+    navigation.navigate('User', { user })
   }
 
   handleAddUser = async () => {
@@ -88,7 +105,7 @@ export default class Main extends Component {
             <Avatar source={{ uri: item.avatar }} />
             <Name>{item.name}</Name>
             <Bio>{item.bio}</Bio>
-            <ProfileButton onPress={() => {}}>
+            <ProfileButton onPress={() => this.handleNavigate(item)}>
               <ProfileButtonText>Ver perfil</ProfileButtonText>
             </ProfileButton>
           </User>
@@ -98,7 +115,3 @@ export default class Main extends Component {
   );
  }
 }
-
-Main.navigationOptions = {
-  title: 'Usuários',
-};
